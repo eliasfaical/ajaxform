418539e93c5288f5dc566cb98e04bfb72ce5ca9b
Elias Faiçal <eliasfaical@gmail.com>
2013-03-28 13:53:24 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
     <head>
-        <title>Formuário Labz</title>
+        <title>Formuário Ajax+PHP</title>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="description" content="">
